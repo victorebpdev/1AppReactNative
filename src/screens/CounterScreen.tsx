518d90d2071b02aa6400b1686d49ea972dae7b78
victor barahona <victorbdev@gmail.com>
2022-06-09 12:14:11 -0400

    first button

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
 
 export const CounterScreen = () => {
+
+  const [count, setCount] = useState(10)
+
   return (
-    <View>
-        <Text>
-            Count: xxxx
+    <View style={{ flex:1, justifyContent:'center'}}>
+        <Text style={{fontSize:45,textAlign:'center'}}>
+            Count: {count}
         </Text>
+        <TouchableOpacity
+         onPress={() => setCount( count + 1 )}
+        >
+            <View style={{ backgroundColor:"#841584", borderRadius:100}}>
+                <Text>CLICK ME</Text>
+
+            </View>
+        </TouchableOpacity>
     </View>
   )
 }
