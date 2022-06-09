cc8cfe852ce6595df9c1fb2225a15056eb16b92a
victor barahona <victorbdev@gmail.com>
2022-06-09 11:46:19 -0400

    first commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { CounterScreen } from './src/screens/CounterScreen';
+import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
+
+export const App = () => {
+  return (
+
+    // <HolaMundoScreen />
+    <CounterScreen/>
+
+  )
+}
+
