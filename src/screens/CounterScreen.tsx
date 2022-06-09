8ade111f45ba5274b4840cedade62872a9e175e0
victor barahona <victorbdev@gmail.com>
2022-06-09 12:45:43 -0400

    btn fab added

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -11,10 +11,21 @@ export const CounterScreen = () => {
             Count: {count}
         </Text>
         <TouchableOpacity
+        style={styles.btnFabLocationBL}
          onPress={() => setCount( count + 1 )}
         >
-            <View style={styles.bottonIncrement}>
-                <Text>CLICK ME</Text>
+            <View style={styles.botttomFab}>
+                <Text style={styles.bottomFabTxt}> + 1</Text>
+
+            </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+        style={styles.btnFabLocationBR}
+         onPress={() => setCount( count - 1 )}
+        >
+            <View style={styles.botttomFab}>
+                <Text style={styles.bottomFabTxt}> - 1</Text>
 
             </View>
         </TouchableOpacity>
@@ -34,5 +45,29 @@ const styles = StyleSheet.create({
     bottonIncrement:{
         backgroundColor:"#841584", 
         borderRadius:100
+    },
+    btnFabLocationBL:{
+        position:'absolute',
+        bottom:25,
+        right:25,
+    },
+    btnFabLocationBR:{
+        position:'absolute',
+        bottom:25,
+        left:25,
+    },
+    botttomFab:{
+        backgroundColor:"#841584", 
+        width:60,
+        height:60,
+        borderRadius:100,
+        justifyContent:'center',
+    },
+    bottomFabTxt:{
+        color: 'Blue',
+        fontSize:25,
+        fontWeight: 'bold',
+        alignSelf:'center'
     }
+    
 })
